8fdcfb03e334fc01b868a21940f7d9146fa184c8
sos801107 <108792640+sos801107@users.noreply.github.com>
2026-04-07 22:00:23 +0800

    Update diy-istoreos-rootfs.sh

--- diy/diy-istoreos-rootfs.sh
+++ diy/diy-istoreos-rootfs.sh
@@ -57,6 +57,8 @@ rm -rf feeds/packages/shadowsocks-libev
 rm -rf feeds/packages/net/shadowsocks-libev
 rm -rf package/feeds/small/shadowsocks-libev
 rm -rf feeds/small/shadowsocks-libev
+rm -rf feeds/small/xray-core
+rm -rf package/feeds/small/xray-core
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
